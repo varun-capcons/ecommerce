4293d3e24b4a2ebcc067d4b283a8b8cdb53b2fac
Varun Chandra <varun@capcons.com>
2022-08-03 11:53:17 +0530

    add pages

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,15 @@
 import Layout from '../components/Layout'
+import ProductItems from '../components/ProductItems'
+import data from '../utils/data'
 
 export default function Home() {
   return (
     <Layout title="Home page">
-        Hello World
+        <div className='grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3'>
+          {data.products.map((product)=> (
+            <ProductItems product={product} key={product.slug} />
+          ))}
+        </div>
     </Layout>
   )
 }
